5abd351ad23e90b8b919a218d781db5accc13e6b
SAROCHA042 <91052520+SAROCHA042@users.noreply.github.com>
2022-08-01 01:03:39 +0700

    Add files via upload

---
+++ Home/sidebar.js
@@ -0,0 +1,10 @@
+// Foreign Menu
+document.getElementById("I-films-work").addEventListener("click", openInterFilmsMenu);
+function openInterFilmsMenu() {
+    document.getElementById("I-films-show").classList.toggle("active");
+}
+// Thai Menu
+document.getElementById("T-films-work").addEventListener("click", openThaiFlimsMenu);
+function openThaiFlimsMenu() {
+    document.getElementById("T-films-show").classList.toggle("active1");
+}
